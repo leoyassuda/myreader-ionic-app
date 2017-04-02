bc7492d6d37529a6625cd7cda457a30f4b9b02af
Leo Yassuda <leo.yassuda@gmail.com>
2017-04-02 00:26:17 -0300

    primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,149 @@
+import {Component} from '@angular/core';
+import {NavController, LoadingController, ActionSheetController} from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map'
+import {InAppBrowser} from 'ionic-native';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+
+export class HomePage {
+
+  public feeds: Array<any>;
+  private url: string = "https://www.reddit.com/new.json";
+  private olderPosts: string = "https://www.reddit.com/new.json?after=";
+  private newerPosts: string = "https://www.reddit.com/new.json?before=";
+
+  public hasFilter: boolean = false;
+  public noFilter: Array<any>;
+
+  constructor(public navCtrl: NavController, public http: Http, public loadingController: LoadingController, public actionSheetController: ActionSheetController) {
+
+    this.fetchContent();
+
+  }
+
+
+  fetchContent(): void {
+    let loading = this.loadingController.create({
+      content: 'Carregando conteúdo...'
+    });
+
+    loading.present();
+
+    this.http.get(this.url).map(res => res.json())
+      .subscribe(data => {
+
+        this.feeds = data.data.children;
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        });
+
+        this.noFilter = this.feeds;
+
+        loading.dismiss();
+
+      });
+  }
+
+  itemSelected(url: string): void {
+    new InAppBrowser(url, '_system');
+  }
+
+  doInfinite(infiniteScroll) {
+    let paramsUrl = (this.feeds.length > 0) ? this.feeds[this.feeds.length - 1].data.name : "";
+
+    this.http.get(this.olderPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+        this.feeds = this.feeds.concat(data.data.children);
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        });
+
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+
+        infiniteScroll.complete();
+
+      })
+  }
+
+  doRefresh(refresher) {
+
+    let paramsUrl = this.feeds[0].data.name;
+
+    this.http.get(this.newerPosts + paramsUrl).map(res => res.json())
+      .subscribe(data => {
+
+        this.feeds = data.data.children.concat(this.feeds);
+
+        this.feeds.forEach((e, i, a) => {
+          if (!e.data.thumbnail || e.data.thumbnail.indexOf('b.thumbs.redditmedia.com') === -1) {
+            e.data.thumbnail = 'http://www.redditstatic.com/icon.png';
+          }
+        });
+
+        this.noFilter = this.feeds;
+        this.hasFilter = false;
+
+        refresher.complete();
+
+      });
+  }
+
+  showFilters(): void {
+    let actionSheet = this.actionSheetController.create({
+      title: 'Opções de filtro:',
+      buttons: [
+        {
+          text: 'Música',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "music");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Filmes',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "movies");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Jogos',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "games");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Fotos',
+          handler: () => {
+            this.feeds = this.noFilter.filter((item) => item.data.subreddit.toLowerCase() === "pics");
+            this.hasFilter = true;
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            this.feeds = this.noFilter;
+            this.hasFilter = false;
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+
+  }
+
+}
